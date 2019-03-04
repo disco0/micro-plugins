a3f5c90badb4e44be07bf81d02658cc6dd56cfed
disco0 <disco0@users.noreply.github.com>
2019-03-03 23:41:24 -0800

    Added pushd to repo path and mkdir for packages output to package.sh

--- package.sh
+++ package.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
+# Go to script path
+pushd "${BASH_SOURCE%/*}/" >/dev/null || exit
+trap "popd >/dev/null 2>&1" EXIT 
+
+# Set packages path
+package_dir="./packages"
+mkdir -p $package_dir || echo -e "\e[38;9mError creating packages output path. [ $package_dir ]"
+
 version="v0.1"
 list=$(ls src)
 for package in $list
 do
-version=$(cat src/$package/repo.json | grep "\"Version\":" | sed s/"\"Version\":"/""/ | sed s/","/""/ | tr "\t" "e" | sed s/"eeee"/""/ | sed s/" "/""/ | sed s/"\""/""/ | sed s/"\""/""/)
-zip -qr9 packages/$package-v$version.zip src/$package
+  version=$(cat src/$package/repo.json | grep "\"Version\":" | sed s/"\"Version\":"/""/ | sed s/","/""/ | tr "\t" "e" | sed s/"eeee"/""/ | sed s/" "/""/ | sed s/"\""/""/ | sed s/"\""/""/)
+  zip -qr9 packages/$package-v$version.zip src/$package
 done
 
+popd >/dev/null
